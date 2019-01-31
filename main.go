5d1d74f19c769b0076e904799e10c918321df1c2
Roberto D'Auria <roberto@measurementlab.net>
2019-01-31 23:02:24 +0000

    Initial code import.

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"log"
+	"os"
+	"strings"
+
+	"cloud.google.com/go/datastore"
+)
+
+const projectID = "mlab-sandbox"
+
+type Credentials struct {
+	Hostname string `datastore:"hostname"`
+	Username string `datastore:"username"`
+	Password string `datastore:"password"`
+	Model    string `datastore:"model"`
+	Address  string `datastore:"address"`
+}
+
+func AddCredentials(ctx context.Context, client *datastore.Client,
+	host string, user string, pass string, model string,
+	ip string) (*datastore.Key, error) {
+	credentials := &Credentials{
+		Hostname: host,
+		Username: user,
+		Password: pass,
+		Model:    model,
+		Address:  ip,
+	}
+
+	key := datastore.IncompleteKey("Credentials", nil)
+	return client.Put(ctx, key, credentials)
+}
+
+func deleteAll(ctx context.Context, client *datastore.Client) {
+	q := datastore.NewQuery("Credentials")
+
+	var creds []*Credentials
+	keys, err := client.GetAll(ctx, q, &creds)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client.DeleteMulti(ctx, keys)
+}
+
+func main() {
+	ctx := context.Background()
+	client, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	deleteAll(ctx, client)
+
+	file, err := os.Open("import")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		splitted := strings.Split(scanner.Text(), " ")
+		_, err := AddCredentials(ctx, client, splitted[0], splitted[1],
+			splitted[2], splitted[3], splitted[4])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if scanner.Err() != nil {
+		log.Printf("Failed to read input file: %v\n", scanner.Err())
+	}
+}
